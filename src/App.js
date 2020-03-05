3b8f426127d334cd21cd168070bcafc9f0b7c8f4
Amber Chunn <a.77@me.com>
2020-03-04 17:59:54 -0800

    Completed Step 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	const [dailyPhoto, setDailyPhoto] = useState({});
+
+	useEffect(() => {
+		axios
+			.get(
+				'https://api.nasa.gov/planetary/apod?api_key=whPRS6eip4SlzNN6SUeDlSoBFnpTUZlrkoiR5Uz7'
+			)
+			.then(response => {
+				setDailyPhoto(response.data);
+				console.log(response.data);
+			})
+			.catch('Error');
+	}, []);
+
+	return (
+		<div className="App">
+			<p></p>
+		</div>
+	);
 }
 
 export default App;
+// whPRS6eip4SlzNN6SUeDlSoBFnpTUZlrkoiR5Uz7;
